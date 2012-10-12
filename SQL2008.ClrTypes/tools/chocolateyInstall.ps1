5a73b326eec2dbf3a29085276677d8d55a93bd31
Iristyle <Iristyle@Github>
2012-10-12 19:15:38 -0400

    Force installing both x86 and x64 on x64 OS
    Both sets of Clr types are needed by dependendencies such as SMO

--- SQL2008.ClrTypes/tools/chocolateyInstall.ps1
+++ SQL2008.ClrTypes/tools/chocolateyInstall.ps1
@@ -11,6 +11,17 @@ try {
 
   Install-ChocolateyPackage @params
 
+  # http://forums.iis.net/p/1174672/1968094.aspx
+  # it turns out that even on x64, x86 clr types should also be installed
+  # or SMO breaks
+  $IsSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
+    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+  if (!$IsSytem32Bit)
+  {
+    $params.url64bit = $params.url
+    Install-ChocolateyPackage @params
+  }
+
   Write-ChocolateySuccess $package
 } catch {
   Write-ChocolateyFailure $package "$($_.Exception.Message)"
