6cdabf1e2d8eefa0d148a11b2c318cfb5e0ad850
Iristyle <Iristyle@github>
2012-10-13 07:59:28 -0400

    SQL 2008 Feature Pack, April 2009

--- SQL2008.SMO/tools/chocolateyInstall.ps1
+++ SQL2008.SMO/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ try {
     packageName = $package;
     fileType = 'msi';
     silentArgs = '/quiet';
-    url = 'http://go.microsoft.com/fwlink/?LinkId=123708&clcid=0x409';
-    url64bit = 'http://go.microsoft.com/fwlink/?LinkId=123709&clcid=0x409';
+    url = 'http://download.microsoft.com/download/0/E/6/0E67502A-22B4-4C47-92D3-0D223F117190/SharedManagementObjects.msi';
+    url64bit = 'http://download.microsoft.com/download/A/D/0/AD021EF1-9CBC-4D11-AB51-6A65019D4706/SharedManagementObjects.msi';
   }
 
   Install-ChocolateyPackage @params
