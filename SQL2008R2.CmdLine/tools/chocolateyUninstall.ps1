9b81d6b9abb2fb31b82a9a2f098e70b1bab048c0
Justin Dearing <zippy1981@gmail.com>
2013-01-17 05:50:45 -0500

    fixed package id for Sql2008R2 commandline.

--- SQL2008R2.CmdLine/tools/chocolateyUninstall.ps1
+++ SQL2008R2.CmdLine/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@
 $msiId = '{5AA08D9F-3BE3-4A34-B53E-858A1895589E}'
 $msiId64 = '{D9F711D3-3C90-4D79-9292-47C90C722E2A}'
 
-$package = 'SQL2008.CmdLine'
+$package = 'SQL2008R2.CmdLine'
 
 try {
   Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId /qb" -validExitCodes @(0)
