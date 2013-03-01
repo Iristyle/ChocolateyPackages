dda4898b0d240c2de7a5beb0cbc61f43f0ed6176
Iristyle <Iristyle@github>
2013-03-01 14:45:50 -0500

    EthanBrown.ChromeDevExtensions 0.0.1
    Includes
    
    Batarang
    Clear Cache
    DevTools AutoSave
    PageSpeed Insights
    Simple REST Client
    Speed Tracer
    CoffeeConsole
    YSlow
    
    Does Not Include
    
    CoffeeScript IDE - Requires login
    PostMan - Requires login
    LiveReload - Not necessary with grunt
    Grunt DevTools - Evaluating
    Tincr - Might be superceded by Chrome workspaces

---
+++ EthanBrown.ChromeDevExtensions/tools/chocolateyInstall.ps1
@@ -0,0 +1,38 @@
+$package = 'EthanBrown.ChromeDevExtensions'
+
+try {
+
+  function Get-CurrentDirectory
+  {
+    $thisName = $MyInvocation.MyCommand.Name
+    [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+  }
+
+  . (Join-Path (Get-CurrentDirectory) 'ChromeHelpers.ps1')
+
+  $toInstall = @{
+    "ighdmehidhipcmcojjgiloacoafjmpfk" = "Batarang";
+    "cppjkneekbjaeellbfkmgnhonkkjfpdn" = "Clear Cache";
+    "mlejngncgiocofkcbnnpaieapabmanfl" = "DevTools AutoSave";
+    "gplegfbjlmmehdoakndmohflojccocli" = "PageSpeed Insights";
+    "fhjcajmcbmldlhcimfajhfbgofnpcjmb" = "Simple REST Client";
+    "ognampngfcbddbfemdapefohjiobgbdl" = "Speed Tracer";
+    "ladbkfdlnaibelfidknofapbbdlhadfp" = "CoffeeConsole";
+    "ninejjcohidippngpapiilnmkgllmakh" = "YSlow";
+    # has messed up install requirements
+    # "hikjpmfebkagiijbdinakadphhjffaik" = "CoffeeScript IDE";
+    # "fdmmgilgnpjigdojojpjoooidkmcomcm" = "PostMan";
+    # handled by Grunt
+    # "jnihajbhpnppcggbcgedagnkighmdlei" = "LiveReload";
+    # deprecated for now
+    # "fbiodiodggnlakggeeckkjccjhhjndnb" = "Grunt DevTools";
+    # "lfjbhpnjiajjgnjganiaggebdhhpnbih" = "Tincr"
+  }
+
+  Install-ChromeExtensions $toInstall
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
