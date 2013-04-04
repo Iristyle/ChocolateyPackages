49cb47cb9da254c1b21191cc18868906bc1d7681
Iristyle <Iristyle@Github>
2013-04-04 09:05:20 -0400

    refactor(sublime pkg control): move install script
     - Will need this for other packages to more safely
        ensure package control is setup

--- SublimeText2.PackageControl/tools/ChocolateyInstall.ps1
+++ SublimeText2.PackageControl/tools/ChocolateyInstall.ps1
@@ -1,19 +1,16 @@
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
 try {
   $package = 'SublimeText2.PackageControl'
 
-  # install package control
-  $installPath = Join-Path $Env:ProgramFiles 'Sublime Text 2'
-  $sublimeDataPath = Join-Path ([Environment]::GetFolderPath('ApplicationData')) 'Sublime Text 2'
-  $packagesPath = Join-Path $sublimeDataPath 'Installed Packages'
-  if (!(Test-Path $packagesPath)) { New-Item $packagesPath -Type Directory }
-  $packageControl = Join-Path $packagesPath 'Package Control.sublime-package'
+  $current = Get-CurrentDirectory
+  . (Join-Path $current 'SublimeHelpers.ps1')
 
-  if (!(Test-Path $packageControl))
-  {
-    # http://wbond.net/sublime_packages/package_control/installation
-    $packageUrl = 'http://sublime.wbond.net/Package%20Control.sublime-package'
-    Get-ChocolateyWebFile -url $packageUrl -fileFullPath $packageControl
-  }
+  Install-SublimePackageControl
 
   Write-ChocolateySuccess $package
 } catch {
--- core/SublimeHelpers.ps1
+++ core/SublimeHelpers.ps1
@@ -1,10 +1,17 @@
 # uses functions in JsonHelpers.ps1
+function Get-SublimeInstallPath
+{
+  Join-Path $Env:ProgramFiles 'Sublime Text 2'
+}
+
+function Get-SublimeSettingsPath
+{
+  Join-Path ([Environment]::GetFolderPath('ApplicationData')) 'Sublime Text 2'
+}
 
 function Get-SublimePackagesPath
 {
-  $installPath = Join-Path $Env:ProgramFiles 'Sublime Text 2'
-  $settingsPath = Join-Path ([Environment]::GetFolderPath('ApplicationData')) 'Sublime Text 2'
-  $packagesPath = Join-Path $settingsPath 'Packages'
+  $packagesPath = Join-Path (Get-SublimeSettingsPath) 'Packages'
   if (!(Test-Path $packagesPath)) { New-Item $packagesPath -Type Directory }
 
   return $packagesPath
@@ -15,6 +22,21 @@ function Get-SublimeUserPath
   $path = Join-Path (Get-SublimePackagesPath) 'User'
   if (!(Test-Path $path)) { New-Item $path -Type Directory }
   return $path
+}
+
+function Install-SublimePackageControl
+{
+  # install package control
+  $packagesPath = Join-Path (Get-SublimeSettingsPath) 'Installed Packages'
+  if (!(Test-Path $packagesPath)) { New-Item $packagesPath -Type Directory }
+  $packageControl = Join-Path $packagesPath 'Package Control.sublime-package'
+
+  if (!(Test-Path $packageControl))
+  {
+    # http://wbond.net/sublime_packages/package_control/installation
+    $packageUrl = 'http://sublime.wbond.net/Package%20Control.sublime-package'
+    Get-ChocolateyWebFile -url $packageUrl -fileFullPath $packageControl
+  }
 }
 
 function Merge-PackageControlSettings
