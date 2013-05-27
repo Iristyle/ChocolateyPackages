0aed9dd19c4e681a3b696875fd7fa6cbda5be373
Iristyle <Iristyle@Github>
2013-05-27 19:27:50 -0400

    feat(AndroidStudio): Init release 0.1.130.677228
     - Based on JetBrains IntelliJ IDEA

---
+++ AndroidStudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$package = 'AndroidStudio'
+
+try {
+  $params = @{
+    PackageName = $package;
+    FileType = 'exe';
+    #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
+    SilentArgs = '/S';
+    Url = 'http://dl.google.com/android/studio/android-studio-bundle-130.677228-windows.exe';
+  }
+
+  Install-ChocolateyPackage @params
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
