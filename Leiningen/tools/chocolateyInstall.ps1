22089e33e18b7463dd555f8e4fb5deeaad4de753
Iristyle <Iristyle@GitHub>
2013-06-06 07:55:30 -0400

    feat: Leiningen 2.2.0
     - Automating Clojure projects without setting your hair on fire

---
+++ Leiningen/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+$package = 'Leiningen'
+$version = '2.2.0'
+
+try {
+  $url = "https://raw.github.com/technomancy/leiningen/$version/bin/lein.bat"
+
+  $batDir = Join-Path $Env:ChocolateyInstall 'bin'
+  $lein = Join-Path $batDir 'lein.bat'
+
+  Write-Host "Downloading from $url"
+
+  $client = New-Object Net.WebClient
+  $client.DownloadFile($url, $lein)
+
+  Write-Host "Download from $url complete"
+
+  Write-Host "Executing bootstrap script from $batDir"
+
+  # $batDir is already in PATH
+  lein self-install
+  lein
+  lein version
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
---
+++ Leiningen/tools/chocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+$package = 'Leiningen'
+$version = '2.2.0'
+
+try {
+  $installDir = Join-Path $Env:USERPROFILE '.lein'
+  if (Test-Path $installDir)
+  {
+    Remove-Item $installDir -Recurse -ErrorAction SilentlyContinue
+  }
+
+  $batDir = Join-Path $Env:ChocolateyInstall 'bin'
+  $lein = Join-Path $batDir 'lein.bat'
+  if (Test-Path $lein)
+  {
+    Remove-Item $lein -ErrorAction SilentlyContinue
+  }
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
