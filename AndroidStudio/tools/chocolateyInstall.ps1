1241d70fb2e5b2ed48db15c455890905bdb00548
Nick Smith <psinutal@gmail.com>
2013-07-15 14:48:47 -0400

    feat: release AndroidStudio 0.2.130.737825
     - will attempt to uninstall 0.1.x if it's found
     see updating instructions here that warn of issues installing over top
     http://developer.android.com/sdk/installing/studio.html

--- AndroidStudio/tools/chocolateyInstall.ps1
+++ AndroidStudio/tools/chocolateyInstall.ps1
@@ -1,15 +1,33 @@
 $package = 'AndroidStudio'
+$version = 130.737825
 
 try {
-  $params = @{
-    PackageName = $package;
-    FileType = 'exe';
-    #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
-    SilentArgs = '/S';
-    Url = 'http://dl.google.com/android/studio/android-studio-bundle-130.677228-windows.exe';
-  }
 
-  Install-ChocolateyPackage @params
+  $build = Join-Path $Env:LOCALAPPDATA 'Android\android-studio\build.txt'
+
+  if ((Test-Path $build) -and ((Get-Content $build) -match '.*?(\d+\.\d+)'))
+  {
+    $installedVersion = [decimal]$Matches[1]
+    if ($installedVersion -lt $version)
+    {
+      Write-Host "Uninstalling existing version $installedVersion"
+      . .\chocolateyUninstall.ps1
+
+      $params = @{
+        PackageName = $package;
+        FileType = 'exe';
+        #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
+        SilentArgs = '/S';
+        Url = 'http://dl.google.com/android/studio/android-studio-bundle-130.737825-windows.exe';
+      }
+
+      Install-ChocolateyPackage @params
+    }
+    else
+    {
+      Write-Host "$package $installedVersion already installed!"
+    }
+  }
 
   Write-ChocolateySuccess $package
 } catch {
--- AndroidStudio/tools/chocolateyUninstall.ps1
+++ AndroidStudio/tools/chocolateyUninstall.ps1
@@ -2,11 +2,6 @@ $package = 'AndroidStudio'
 
 try
 {
-  $installPath = (Join-Path "${Env:\ProgramFiles(x86)}" 'AndroidStudio'),
-    (Join-Path 'Env:ProgramFiles' 'AndroidStudio') |
-    ? { Test-Path $_ } |
-    Select -First 1
-
   $uninstall = Join-Path $Env:LOCALAPPDATA 'Android\android-studio\uninstall.exe'
 
   #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
