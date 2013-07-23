aabdb2b6ca74b7122e6cd676ad24811ff8e1b3ea
Iristyle <Iristyle@Github>
2013-07-23 17:01:20 -0400

    feat: Sublime Text 3 package control 2.0.0-alpha9
     - requires a git clone instead of a simple package file download now

---
+++ SublimeText3.PackageControl/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+try {
+  $package = 'SublimeText3.PackageControl'
+
+  $current = Get-CurrentDirectory
+  . (Join-Path $current 'SublimeHelpers.ps1')
+
+  Install-SublimePackageControl -Version 3
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
