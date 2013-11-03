a0492c1ee372a3375657db95cfb2f325e4aa45ec
edmund <the.edmund@gmail.com>
2013-11-03 17:10:06 +0000

    Added btsync package

---
+++ btsync/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'BitTorrent Sync'
+$installerType = 'exe'
+$url = 'http://download-lb.utorrent.com/endpoint/btsync/os/windows/track/stable'
+#$url64 = $url No 64-bit version available
+$silentArgs = '/PERFORMINSTALL /AUTOMATION'
+$validExitCodes = @(0,1) # Returns 1 after a successful automated install, naturally
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
