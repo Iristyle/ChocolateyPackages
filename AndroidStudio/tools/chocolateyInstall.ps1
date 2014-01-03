7d5a9879440ba440fcf4dfc07982484ff3761dfc
Iristyle <Iristyle@Github>
2014-01-03 12:18:34 -0500

    chore: release Android Studio 0.3.2 Build 132
     - Fix a real dumb bug in the installer that didn't run if Android Studio
       wasn't previously installed

--- AndroidStudio/tools/chocolateyInstall.ps1
+++ AndroidStudio/tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 $package = 'AndroidStudio'
-$version = 130.737825
+$majorVersion = '0.3.2'
+$buildVersion = 132.893413
 
 try {
 
@@ -8,26 +9,27 @@ try {
   if ((Test-Path $build) -and ((Get-Content $build) -match '.*?(\d+\.\d+)'))
   {
     $installedVersion = [decimal]$Matches[1]
-    if ($installedVersion -lt $version)
+    if ($installedVersion -lt $buildVersion)
     {
       Write-Host "Uninstalling existing version $installedVersion"
       . .\chocolateyUninstall.ps1
-
-      $params = @{
-        PackageName = $package;
-        FileType = 'exe';
-        #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
-        SilentArgs = '/S';
-        Url = 'http://dl.google.com/android/studio/android-studio-bundle-130.737825-windows.exe';
-      }
-
-      Install-ChocolateyPackage @params
     }
     else
     {
       Write-Host "$package $installedVersion already installed!"
+      exit
     }
   }
+
+  $params = @{
+    PackageName = $package;
+    FileType = 'exe';
+    #uses NSIS installer - http://nsis.sourceforge.net/Docs/Chapter3.html
+    SilentArgs = '/S';
+    Url = "https://dl.google.com/android/studio/install/$majorVersion/android-studio-bundle-$buildVersion-windows.exe";
+  }
+
+  Install-ChocolateyPackage @params
 
   Write-ChocolateySuccess $package
 } catch {
