d06f78a26fa8c8afb76d7140010b435062f21dd3
Luke Hoffmann <lakehoffmann@gmail.com>
2014-01-31 12:14:36 +1030

    new URL for standalone installer, simplified package without Adobe AIR

--- HipChat/tools/chocolateyInstall.ps1
+++ HipChat/tools/chocolateyInstall.ps1
@@ -4,30 +4,13 @@ try {
 
   $hipParams = @{
     PackageName = $package;
-    FileFullPath = (Join-Path $Env:TEMP 'hipchat.air');
-    Url = 'http://downloads.hipchat.com/hipchat.air'
+    FileType = 'msi';
+    SilentArgs = '/qn';
+    Url = 'https://www.hipchat.com/downloads/latest/qtwindows'
   }
-
-  Get-ChocolateyWebFile @hipParams
-
-  $airInstall = 'Adobe AIR\Versions\1.0\Adobe AIR Application Installer.exe'
-  $airPath = $Env:CommonProgramFiles, ${Env:CommonProgramFiles(x86)} |
-    % { Join-Path $_ $airInstall } |
-    ? { Test-Path $_ } |
-    Select -First 1
-
-  if (!$airPath)
-  {
-    Write-ChocolateyFailure $package 'Could not find AIR installer!'
-    return
-  }
-
-  $installPath = Join-Path $Env:ProgramFiles 'Hipchat'
-  $airParams = @('-silent', '-eulaAccepted', '-programMenu',
-     '-location', "`"$installPath`"", "`"$($hipParams.FileFullPath)`"")
-
-  Start-ChocolateyProcessAsAdmin -exeToRun $airPath -statements $airParams
-
+    
+  Install-ChocolateyPackage @hipParams
+ 
   Write-ChocolateySuccess $package
 } catch {
   Write-ChocolateyFailure $package "$($_.Exception.Message)"
