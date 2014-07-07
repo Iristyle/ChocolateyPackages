a5476ccb2875cb1c1988839782743ce4443ceee3
Onorio Catenacci <OnorioCatenacci@quickenloans.com>
2014-07-07 13:24:30 -0400

    Upgrading the package for Erlang/OTP 17.1

--- Erlang/tools/chocolateyInstall.ps1
+++ Erlang/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿try {
-	Install-ChocolateyPackage 'erlang' 'EXE' '/S' 'http://www.erlang.org/download/otp_win32_17.0.exe' 'http://www.erlang.org/download/otp_win64_17.0.exe'  -validExitCodes @(0)
+	Install-ChocolateyPackage 'erlang' 'EXE' '/S' 'http://www.erlang.org/download/otp_win32_17.1.exe' 'http://www.erlang.org/download/otp_win64_17.1.exe'  -validExitCodes @(0)
 
   Write-ChocolateySuccess 'Erlang'
 } catch {
