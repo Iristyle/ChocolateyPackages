a23a7a8056311fe8c9d76529cbd450065ac30e68
thedeetch <thedeetch@users.noreply.github.com>
2015-02-27 15:02:55 -0600

    Fixing proxy install.  Updating to 150224a.

--- ConEmu/tools/chocolateyInstall.ps1
+++ ConEmu/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $package = 'ConEmu'
-$version = '14.09.23'
+$version = '15.02.24a'
 
 try {
 
@@ -11,33 +11,17 @@ try {
   # TODO: use github api to grab latest release?
   $url = "https://github.com/Maximus5/ConEmu/releases/download/v$version/ConEmuSetup.$($version.replace('.','')).exe"
 
-  $chocTemp = Join-Path $Env:TEMP 'chocolatey'
-  $tempInstall = Join-Path $chocTemp "ConEmu\ConEmuSetup.$version.exe"
+  echo $url
 
-  Write-Host "Downloading from $url to $tempInstall"
-
-  # need a Referer, User-Agent and Accept to be able to download
-  # other headers not required
-  $client = New-Object Net.WebClient
-  $client.DownloadFile($url, $tempInstall)
-
-  Write-Host "Download from $url complete"
-
-  # If having problems with untrusted cetrificates on HTTPS, use
-  # solution: http://stackoverflow.com/a/561242/1579985
   $params = @{
     PackageName = $package;
     FileType = 'exe';
     SilentArgs = "/p:$os /passive";
-    # MSI installer, but packed inside wrapper to select x86 or x64
-    # version. Therefore, treat it as EXE type.
-    File = $tempInstall;
-    # ValidExitCodes = @(0);
+    Url = $url;
+    Url64Bit = $url;
   }
 
-  Install-ChocolateyInstallPackage @params
-
-  Write-ChocolateySuccess $package
+  Install-ChocolateyPackage @params
 } catch {
   Write-ChocolateyFailure $package "$($_.Exception.Message)"
   throw
