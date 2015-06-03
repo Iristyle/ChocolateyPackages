19d10d6c11db25c9ac60640c8cb30a88439a8788
Michael Ray <michael.ray@rayzor.net>
2015-06-03 01:40:16 -0500

    Update to not error with latest chocolatey version
    The VBoxManage install command was returning the percentage progress via
    stderr, which caused chocolatey to assume failure (even though it did
    install the ExtensionPack successfully). This fix captures the
    stdout/stderr, then either displays it on success or uses it as an
    exception message on failure, which causes chocolatey to correctly
    determine success or failure of the VB ExtensionPack installation.

--- VirtualBox.ExtensionPack/tools/chocolateyUninstall.ps1
+++ VirtualBox.ExtensionPack/tools/chocolateyUninstall.ps1
@@ -1,50 +1,37 @@
 $package = 'VirtualBox.ExtensionPack'
 $vboxName = 'Oracle VM VirtualBox Extension Pack'
 
-try {
-  # simulate the unix command for finding things in path
-  # http://stackoverflow.com/questions/63805/equivalent-of-nix-which-command-in-powershell
-  function Which([string]$cmd)
+# simulate the unix command for finding things in path
+# http://stackoverflow.com/questions/63805/equivalent-of-nix-which-command-in-powershell
+function Which([string]$cmd)
+{
+  Get-Command -ErrorAction "SilentlyContinue" $cmd |
+    Select -ExpandProperty Definition
+}
+
+function Uninstall-ExtensionPack([string] $name)
+{
+  $vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
+
+  $vboxManage = (Which VBoxManage),
+    $vboxManageDefault |
+    ? { Test-Path $_ } |
+    Select -First 1
+
+  if (!$vboxManage)
   {
-    Get-Command -ErrorAction "SilentlyContinue" $cmd |
-      Select -ExpandProperty Definition
+    throw 'Could not find VirtualBox VBoxManage.exe necessary to uninstall extension pack'
   }
 
-  function Uninstall-ExtensionPack([string] $name)
-  {
-    $vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
-
-    $vboxManage = (Which VBoxManage),
-      $vboxManageDefault |
-      ? { Test-Path $_ } |
-      Select -First 1
-
-    if (!$vboxManage)
-    {
-      throw 'Could not find VirtualBox VBoxManage.exe necessary to install extension pack'
-    }
-
-    &$vBoxManage extpack uninstall `"$name`"
-  }
-
-  Uninstall-ExtensionPack $vboxName
-
+  $vboxout = & $vBoxManage extpack uninstall `"$name`" 2>&1
   if ($LASTEXITCODE -ne 0)
   {
-    Write-ChocolateyFailure $package @"
-Due to a VirtualBox bug, VBoxManage appears unresponsive.
-
-Please reboot the machine, and attempt to uninstall the VirtualBox extension pack
-using the command line:
-
-VBoxManage extpack uninstall `"$vboxName`"
-"@
+    throw "An error occurrred with VirtualBox VBoxManage.exe uninstall command: $vboxout"
   }
   else
   {
-    Write-ChocolateySuccess $package
+    Write-Output "$vboxout"
   }
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
 }
+
+Uninstall-ExtensionPack $vboxName
