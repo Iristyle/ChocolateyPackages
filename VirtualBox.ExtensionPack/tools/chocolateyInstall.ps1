87ab9547c6f06858dcb324493362e2382e96467f
Michael Ray <michael.ray@rayzor.net>
2015-08-18 14:33:38 -0500

    VirtualBox.ExtensionPack: No longer use functions
    Remove the use of functions in the Chocolatey install/uninstall scripts.
    Since these scripts are generally pretty simple and nothing is being
    called more than once and also is not modularized, it is probably simplest
    to not use functions at all.

--- VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
+++ VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
@@ -4,48 +4,38 @@ $build = '101573'
 $packName = "Oracle_VM_VirtualBox_Extension_Pack-$version-$build.vbox-extpack"
 $packUrl = "http://download.virtualbox.org/virtualbox/$version/$packName"
 
+$vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
+
 # simulate the unix command for finding things in path
 # http://stackoverflow.com/questions/63805/equivalent-of-nix-which-command-in-powershell
-function Which([string]$cmd)
+$vboxManage = (Get-Command -ErrorAction "SilentlyContinue" VBoxManage |
+  Select -ExpandProperty Definition),
+  $vboxManageDefault |
+  ? { $_ -and { Test-Path $_ } } |
+  Select -First 1
+
+if (!$vboxManage)
 {
-  Get-Command -ErrorAction "SilentlyContinue" $cmd |
-    Select -ExpandProperty Definition
+  throw 'Could not find VirtualBox VBoxManage.exe to install extension pack with'
 }
 
-function Install-ExtensionPack([string] $url)
+$fileName = $packUrl -split '/' | Select -Last 1
+$appTemp = Join-Path $Env:Temp $package
+if (!(Test-Path $appTemp))
 {
-  $vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
-
-  $vboxManage = (Which VBoxManage),
-    $vboxManageDefault |
-    ? { $_ -and { Test-Path $_ } } |
-    Select -First 1
-
-  if (!$vboxManage)
-  {
-    throw 'Could not find VirtualBox VBoxManage.exe to install extension pack with'
-  }
-
-  $fileName = $url -split '/' | Select -Last 1
-  $appTemp = Join-Path $Env:Temp $package
-  if (!(Test-Path $appTemp))
-  {
-    New-Item $appTemp -Type Directory
-  }
-  $packageTemp = Join-Path $appTemp $fileName
-  Get-ChocolateyWebFile -packageName $package -fileFullPath $packageTemp -url $url
-
-  Push-Location $appTemp
-  $vboxout = & $vboxManage extpack install --replace $packName 2>&1
-  if ($LASTEXITCODE -ne 0)
-  {
-    throw "An error occurrred with VirtualBox VBoxManage.exe install command: $vboxout"
-  }
-  else
-  {
-    Write-Output "$vboxout"
-  }
-  Pop-Location
+  New-Item $appTemp -Type Directory
 }
+$packageTemp = Join-Path $appTemp $fileName
+Get-ChocolateyWebFile -packageName $package -fileFullPath $packageTemp -url $packUrl
 
-Install-ExtensionPack $packUrl
+Push-Location $appTemp
+$vboxout = & $vboxManage extpack install --replace $packName 2>&1
+if ($LASTEXITCODE -ne 0)
+{
+  throw "An error occurrred with VirtualBox VBoxManage.exe install command: $vboxout"
+}
+else
+{
+  Write-Output "$vboxout"
+}
+Pop-Location
--- VirtualBox.ExtensionPack/tools/chocolateyUninstall.ps1
+++ VirtualBox.ExtensionPack/tools/chocolateyUninstall.ps1
@@ -1,37 +1,27 @@
 $package = 'VirtualBox.ExtensionPack'
 $vboxName = 'Oracle VM VirtualBox Extension Pack'
 
+$vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
+
 # simulate the unix command for finding things in path
 # http://stackoverflow.com/questions/63805/equivalent-of-nix-which-command-in-powershell
-function Which([string]$cmd)
+$vboxManage = (Get-Command -ErrorAction "SilentlyContinue" VBoxManage |
+    Select -ExpandProperty Definition),
+  $vboxManageDefault |
+  ? { Test-Path $_ } |
+  Select -First 1
+
+if (!$vboxManage)
 {
-  Get-Command -ErrorAction "SilentlyContinue" $cmd |
-    Select -ExpandProperty Definition
+  throw 'Could not find VirtualBox VBoxManage.exe necessary to uninstall extension pack'
 }
 
-function Uninstall-ExtensionPack([string] $name)
+$vboxout = & $vBoxManage extpack uninstall `"$vboxName`" 2>&1
+if ($LASTEXITCODE -ne 0)
 {
-  $vboxManageDefault = Join-Path $Env:ProgramFiles 'Oracle\VirtualBox\VBoxManage.exe'
-
-  $vboxManage = (Which VBoxManage),
-    $vboxManageDefault |
-    ? { Test-Path $_ } |
-    Select -First 1
-
-  if (!$vboxManage)
-  {
-    throw 'Could not find VirtualBox VBoxManage.exe necessary to uninstall extension pack'
-  }
-
-  $vboxout = & $vBoxManage extpack uninstall `"$name`" 2>&1
-  if ($LASTEXITCODE -ne 0)
-  {
-    throw "An error occurrred with VirtualBox VBoxManage.exe uninstall command: $vboxout"
-  }
-  else
-  {
-    Write-Output "$vboxout"
-  }
+  throw "An error occurrred with VirtualBox VBoxManage.exe uninstall command: $vboxout"
 }
-
-Uninstall-ExtensionPack $vboxName
+else
+{
+  Write-Output "$vboxout"
+}
