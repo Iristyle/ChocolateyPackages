a04553ed4c37d3a10fb94f20f714bb8e5c422e75
Michael Ray <michael.ray@rayzor.net>
2015-10-20 20:47:42 -0500

    VirtualBox.ExtensionPack: Update to v5.0.8.103449
    Update the VirtualBox.ExtensionPack chocolatey package from v5.0.6.103037
    to v5.0.8.103449.

--- VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
+++ VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$package = 'VirtualBox.ExtensionPack'
-$version = '5.0.6'
-$build = '103037'
+$version = '5.0.8'
+$build = '103449'
 $packName = "Oracle_VM_VirtualBox_Extension_Pack-$version-$build.vbox-extpack"
 $packUrl = "http://download.virtualbox.org/virtualbox/$version/$packName"
 
