904a4cc5b916fdb899a00242a411cfcc16e03d1e
Michael Ray <michael.ray@rayzor.net>
2016-01-22 21:49:14 -0600

    VirtualBox.ExtensionPack: Update to v5.0.14.105127
    Update the VirtualBox.ExtensionPack chocolatey package from v5.0.12.104815
    to v5.0.14.105127.

--- VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
+++ VirtualBox.ExtensionPack/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$package = 'VirtualBox.ExtensionPack'
-$version = '5.0.12'
-$build = '104815'
+$version = '5.0.14'
+$build = '105127'
 $packName = "Oracle_VM_VirtualBox_Extension_Pack-$version-$build.vbox-extpack"
 $packUrl = "http://download.virtualbox.org/virtualbox/$version/$packName"
 
